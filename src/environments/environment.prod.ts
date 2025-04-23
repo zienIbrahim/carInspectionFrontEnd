0c943b28943d729574e48de7e0b4becd0ac884e1
zainalabdin <zainal3abden@gmail.com>
2025-04-24 00:02:58 +0300

    []

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl:"https://192.168.8.93:7008/api/"
+  apiUrl:"https://app.redaimond.com:7008/api/"
 
 };
